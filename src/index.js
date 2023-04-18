5518b6da56d81107b4a53bcf1c6c7088139f16ff
Cenk Erdonmez <cenkerdonmez@gmail.com>
2023-04-18 12:47:48 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  form.addEventListener("click",(e)=>{
+    e.preventDefault,
+  })
 });
+
+const form= document.getElementById("create-task-form")
